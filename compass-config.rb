ce255e686372a098c2fd2214a1508d2e263e0a13
Alex Batsuev <batsuev@gmail.com>
2014-09-15 12:49:57 +0900

    remove bootstrap

--- compass-config.rb
+++ compass-config.rb
@@ -1,9 +1,6 @@
-# Require any additional compass plugins here.
-require 'bootstrap-sass'
-
 # Set this to the root of your project when deployed:
 http_path = "/"
-sass_dir = "src/scss"
+sass_dir = "src-scss"
 public_root = "resources/public/dev/"
 css_dir = "#{public_root}css"
 images_dir = "#{public_root}img"
