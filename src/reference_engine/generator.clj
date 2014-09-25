7bc49a133a928c25afefdc81690f53c0a7deb03c
Alex Batsuev <batsuev@gmail.com>
2014-09-26 01:01:30 +0900

    parallel jsdoc

--- src/reference_engine/generator.clj
+++ src/reference_engine/generator.clj
@@ -12,11 +12,31 @@
 
 (def local (atom {}))
 
+(defn get-jsdoc-recursive [path]
+  (parse-string
+   ((sh "/usr/local/bin/node" "./node_modules/jsdoc/jsdoc.js" "-r" "-X" path) :out)
+   true))
+
+(defn get-jsdoc [path]
+  (parse-string
+   ((sh "/usr/local/bin/node" "./node_modules/jsdoc/jsdoc.js" "-X" path) :out)
+   true))
+
 (defn get-jsdoc-info [path]
-  (println "Getting jsdoc info")
-  (time (parse-string
-         ((sh "/usr/local/bin/node" "./node_modules/jsdoc/jsdoc.js" "-r" "-X" path) :out)
-         true)))
+  (println "running jsdoc -x")
+  (time
+   (let [folders (conj
+                  (filter #(and (.isDirectory %)
+                                (not (.isHidden %)))
+                          (vec (.listFiles (file path))))
+                  (file path))]
+     (apply concat (doall (pmap
+                           (fn [folder]
+                             (let [p (.getAbsolutePath folder)]
+                               (if (= p path)
+                                 (get-jsdoc p)
+                                 (get-jsdoc-recursive p)))
+                             ) folders))))))
 
 (defn generate-local [path]
   (let [data (jsdoc-parser/parse (get-jsdoc-info path)
