45c1724a74139b729b00ca1f60e6835343d4c15b
Alex Batsuev <batsuev@gmail.com>
2014-12-01 17:32:23 +0800

    submodules, сука

--- src/reference/generator/git.clj
+++ src/reference/generator/git.clj
@@ -2,6 +2,7 @@
   (:require [clj-jgit.porcelain :as git]
             [clj-jgit.querying :as gitq]
             [reference.config :as config]
+            [clojure.java.shell :refer [sh]]
             [clojure.java.io :refer [file]]))
 
 (defmacro auth-git [ & body ]
@@ -11,21 +12,28 @@
              git/*ssh-pubkey* (slurp (str config/keys-path "id_rsa.pub"))]
      ~@body))
 
-(defn- get-repo [project]
-  (git/load-repo (str config/data-path project "/repo")))
+(defn- get-repo []
+  (git/load-repo (str config/data-path "/repo")))
 
 (defn- branches [repo]
   (gitq/branch-list-with-heads repo))
 
-(defn- checkout-branch [[branch commit] project repo]
-  (println (str config/data-path project "/versions/"))
-  (if-let [short-name (last (re-find #"refs/heads/(.*)$" (.getName branch)))]
-    (git/git-clone (str config/data-path project "/repo/.git")
-                   (str config/data-path project "/versions/" short-name)
-                   "origin"
-                   short-name))
-  (println "branch:" (.getName branch))
-  (println "commit:" (.getName commit)))
+(defn- get-branch-name [branch]
+  (last (re-find #"refs/heads/(.*)$" (.getName branch))))
+
+(defn- checkout-branch [[branch commit] repo]
+  (if-let [short-name (get-branch-name branch)]
+    (do
+      (println "checkouting" short-name)
+      (sh "copy" "-r"
+          (str config/data-path "/repo")
+          (str config/data-path "/versions/" short-name))
+      (git/git-checkout (git/load-repo (str config/data-path "/versions/" short-name))
+                        short-name))))
+
+(defn update-submodules [short-name]
+  (let [repo (git/load-repo (str config/data-path "/versions/" short-name))]
+        (git/git-submodule-update repo)))
 
 (defn- delete-recursively [fname]
   (let [func (fn [func f]
@@ -35,14 +43,20 @@
                (clojure.java.io/delete-file f))]
     (func func (file fname))))
 
-(defn update [project]
-  (let [repo (get-repo project)]
+(defn update [branches-filter]
+  (let [repo (get-repo)]
     (auth-git
      (git/git-fetch-all repo)
-     (let [versions-base-path (str config/data-path project "/versions")
-           branches (branches repo)]
+     (git/git-submodule-sync repo)
+     (println "===")
+     (println (git/git-submodule-update repo))
+     (let [versions-base-path (str config/data-path "/versions")
+           branches (filter (fn [[branch commit]]
+                              (branches-filter (get-branch-name branch)))
+                            (branches repo))]
        (if (.exists (file versions-base-path))
          (delete-recursively versions-base-path))
-       (map #(checkout-branch % project repo) branches)))))
-
-(update "acdvf")
+       (doall (map #(checkout-branch % repo) branches))
+       (map (fn [[branch commit]]
+              {:name (get-branch-name branch)
+               :commit (.getName commit)}) branches)))))
--- src/reference/generator/versions.clj
+++ src/reference/generator/versions.clj
@@ -1,5 +1,7 @@
 (ns reference.generator.versions
-  (:require [taoensso.carmine :as car :refer (wcar)]))
+  (:require [taoensso.carmine :as car :refer (wcar)]
+            [reference.generator.git :as git]
+            [cheshire.core :refer [generate-string]]))
 
 (def server-conn {:pool {} :spec {:host "127.0.0.1" :port 6379}})
 (defmacro wcar* [& body] `(car/wcar server-conn ~@body))
@@ -13,5 +15,13 @@
 (defn- redis-version-build-hash-key [version]
   (str "ref-version-" version "-hash"))
 
+(defn force-build-version [version]
+  (if-let [branch (git/update #{version})]
+    (let [a 1]
+      (wcar* (car/sadd (redis-versions-key) version)
+             (car/set (redis-version-build-hash-key version) (:commit branch))))
+    (println "branch " version " not found")))
 
 (defn build [])
+
+(force-build-version "develop")
