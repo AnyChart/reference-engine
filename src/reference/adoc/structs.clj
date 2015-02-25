15cf1802514f74680704b9335123cd58ec235eab
batsuev <batsuev@gmail.com>
2015-02-25 15:48:25 +0800

    fix structurization

--- src/reference/adoc/structs.clj
+++ src/reference/adoc/structs.clj
@@ -1,6 +1,14 @@
 (ns reference.adoc.structs
   (:require [taoensso.timbre :as timbre :refer [info]]))
 
+(defn- parse-description [description]
+  description)
+
+(defn- cleanup-name [name]
+  (if name
+    (clojure.string/replace (str name) #"['\"]" "")
+    nil))
+
 (defn- get-doclets-by-kind [doclets kind]
   (filter #(= kind (:kind %)) doclets))
 
@@ -18,36 +26,91 @@
 (defn- is-static [entry]
   (= (:scope entry) "static"))
 
-(defn- group-functions [functions])
+(defn- get-tag [doclet tag]
+  (filter #(= (:title %) tag) (:tags doclet)))
 
-(defn- create-typedef [typedef doclets])
+(defn- parse-general [entry]
+  {:name (:name entry)
+   :description (parse-description (:description entry))
+   :full-name (cleanup-name (:longname entry))
+   :since (:since entry)})
 
-(defn- create-enum [enum doclets])
+(defn- parse-examples-and-listing [entry doclet]
+  (let [samples (:examples doclet)
+        listings (get-tag doclet "listing")]
+    (assoc entry
+           :samples samples
+           :has-samples (boolean (seq samples))
+           :listings (map :value listings)
+           :has-listings (boolean (seq (listings))))))
 
-(defn- create-constant [const doclets])
+(defn- group-functions [functions]
+  (reduce (fn [res val]
+            (let [name (:name val)
+                  group (get res name)]
+              (assoc res name (if group
+                                (conj group val)
+                                [val]))) {} functions)))
 
-(defn- create-function [func doclets])
+(defn- create-typedef-property [prop]
+  (assoc (parse-general prop)
+         :type (get-in prop [:type :names])))
+
+(defn- create-typedef [typedef doclets]
+  (assoc (parse-examples-and-listing (parse-general typedef) typedef)
+         :properties (map create-typedef-property (:properties typedef))
+         :type (get-in prop [:type :names])))
+
+(defn- create-enum-field [doclet]
+  (assoc (parse-general doclet)
+         :default (:defaultvalue doclet)))
+
+(defn- get-enum-fields [enum doclets]
+  (map create-enum-field (get-doclets-with-filter enum "member" is-static)))
+
+(defn- create-enum [enum doclets]
+  (assoc (parse-examples-and-listing (parse-general enum) enum)
+         :fields (get-enum-fields enum doclets)))
+
+(defn- create-constant [const doclets]
+  (parse-examples-and-listing (parse-general const) const))
+
+(defn- parse-function-return [ret]
+  {:types (get-in ret [:type :names])
+   :description (:description ret)})
+
+(defn- parse-function-param [param]
+  (assoc (parse-function-return param)
+         :name (:name param)))
+
+(defn- create-function [func doclets]
+  (assoc (parse-examples-and-listing (parse-general func) func)
+         :detailed-desc (:value (first (get-tag func "detailed")))
+         :params (map parse-function-param (:params func))
+         :returns (map parse-function-return (:returns func))))
 
 ;; class
 ;; - class
 ;; - enum
 ;; - function (non-static!)
 (defn- create-class [class doclets]
-  {:doclet class
-   :enums (map #(create-enum % doclets)
-               (get-doclets-with-filter doclets
-                                        class
-                                        "member"
-                                        #(:isEnum %)))
-   :classes (map #(create-class % doclets)
-                 (get-doclets-by-memberof-and-kind doclets
-                                                   namespace
-                                                   "class"))
-   :functions (group-functions (map #(create-function % doclets)
-                                    (get-doclets-with-filter doclets
-                                                             namespace
-                                                             "function"
-                                                             #(not (is-static %)))))})
+  (assoc (parse-general class)
+         :extends (:augments class)
+         :has-extends (boolean (seq (:aumgnets class)))
+         :enums (map #(create-enum % doclets)
+                     (get-doclets-with-filter doclets
+                                              class
+                                              "member"
+                                              #(:isEnum %)))
+         :classes (map #(create-class % doclets)
+                       (get-doclets-by-memberof-and-kind doclets
+                                                         namespace
+                                                         "class"))
+         :functions (group-functions (map #(create-function % doclets)
+                                          (get-doclets-with-filter doclets
+                                                                   namespace
+                                                                   "function"
+                                                                   #(not (is-static %)))))))
 
 ;; namespace:
 ;; - namespace
@@ -57,7 +120,7 @@
 ;; - const
 ;; - static function
 (defn- create-namespace [namespace doclets]
-  {:doclet namespace
+  {:info (parse-general namespace)
    :typedefs (map #(create-typedef % doclets)
                   (get-doclets-by-memberof-and-kind doclets
                                                     namespace
