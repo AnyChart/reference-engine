88c8f53bb0b3384934edab7270001e09201ff19e
batsuev <batsuev@gmail.com>
2015-06-16 12:52:42 +0800

    database base functions and struct

--- src/reference/data/pages.clj
+++ src/reference/data/pages.clj
@@ -1,41 +1,24 @@
 (ns reference.data.pages
-  (:require [reference.config :as config]
-            [clojure.java.io :refer [as-file]]
-            [taoensso.carmine :as car :refer (wcar)]))
+  (:require [reference.components.jdbc :refer [query one insert! exec]]))
 
-(def server-conn {:pool {} :spec {:host "127.0.0.1" :port 6379}})
-(defmacro wcar* [& body] `(car/wcar server-conn ~@body))
+;; CREATE SEQUENCE page_id_seq;
+;; CREATE TYPE page_type AS ENUM ('namespace', 'class', 'typedef', 'enum');
+;; CREATE TABLE pages (
+;;   id integer PRIMARY KEY DEFAULT nextval('page_id_seq'),
+;;   type page_type,
+;;   version_id integer references versions(id),
+;;   url varchar(255) not null,
+;;   content text
+;; )
 
-(defn- get-path [version page]
-  (str config/data-path "versions-data/" version "/" page ".html"))
+(defn page-by-url [version-id page-url]
+  (one jdbc (-> (select :*)
+                (from :samples)
+                (where [:= :version_id version-id]
+                       [:= :url url]))))
 
-(defn page-exists? [version page]
-  (let [path (get-path version page)]
-    (.exists (as-file path))))
-
-(defn get-page [version page]
-  (slurp (get-path version page)))
-
-(defn- redis-version-page-info-key [version page]
-  (str "ref-version-" version "-" page "-info"))
-
-(defn- save-page-info [version page info]
-  (wcar* (car/set (redis-version-page-info-key version page)
-                  info)))
-
-(defn page-info [version page]
-  (wcar* (car/get (redis-version-page-info-key version page))))
-
-(defn add-top-level [version entries]
-  (doall (map #(save-page-info version
-                               (:full-name %)
-                               {:kind :namespace}) (:namespaces entries)))
-  (doall (map #(save-page-info version
-                               (:full-name %)
-                               {:kind :class}) (:classes entries)))
-  (doall (map #(save-page-info version
-                               (:full-name %)
-                               {:kind :typedef}) (:typedefs entries)))
-  (doall (map #(save-page-info version
-                               (:full-name %)
-                               {:kind :enum}) (:enums entries))))
+(defn add-page [version-id type url content]
+  (insert! jdbc :pages {:url url
+                        :type type
+                        :content content
+                        :version_id version-id}))
