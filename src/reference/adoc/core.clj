741135e46887d076e0eccf3be1cabeae9280b688
batsuev <batsuev@gmail.com>
2015-06-22 11:51:42 +0800

    add logging

--- src/reference/adoc/core.clj
+++ src/reference/adoc/core.clj
@@ -30,8 +30,11 @@
     (vdata/delete-by-id jdbc version-id)))
 
 (defn- remove-branches [jdbc actual-branches]
+  (info "actual branches" actual-branches)
   (let [current-branches (vdata/versions jdbc)
         removed-branches (filter #(some #{%} actual-branches) current-branches)]
+    (info "current branches" current-branches)
+    (info "removed branches" removed-branches)
     (if (seq removed-branches)
       (doall (map #(remove-branch jdbc %) removed-branches)))
     removed-branches))
