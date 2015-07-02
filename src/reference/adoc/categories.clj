18c77fb7d4cb27b7dcd0f1209617f3330c97be65
batsuev <batsuev@gmail.com>
2015-07-02 15:25:51 +0800

    class overrides categorization

--- src/reference/adoc/categories.clj
+++ src/reference/adoc/categories.clj
@@ -2,10 +2,10 @@
   (:require [taoensso.timbre :as timbre :refer [info]]))
 
 (defn- get-method-category [method]
-  (first (reduce (fn [res override]
-                   (if (:has-category override)
-                     (conj res (:category override))
-                     res)) [] (:overrides method))))
+  (set (doall (reduce (fn [res override]
+                        (if (:has-category override)
+                          (conj res (:category override))
+                          res)) [] (:overrides method)))))
 
 (defn- get-class-categories [methods]
   (set (doall (reduce (fn [res method]
@@ -20,7 +20,7 @@
                           res))
                       [] functions))))
 
-(defn- categorize-members [members]
+(defn- categorize-namespace-functions [members]
   (doall (map (fn [[k v]] v)
               (reduce (fn [res member]
                         (let [category (if (:has-category member)
@@ -32,16 +32,34 @@
                       {}
                       members))))
 
+(defn- categorize-class-methods [methods]
+  (doall (map (fn [[k v]] v)
+              (reduce (fn [res method]
+                        (reduce (fn [res override]
+                                  (let [category (if (:has-category override)
+                                                   (:category override)
+                                                   "Uncategorized")
+                                        data (get res category {:name category :members []})]
+                                    (assoc res category
+                                           (assoc data :members
+                                                  (conj (:members data) override)))))
+                                res
+                                (:overrides method)))
+                      {}
+                      methods))))
+
 (defn build-class-categories [class]
-  (let [categories (get-class-categories (:methods class))]
+  (let [categories (get-class-categories (:methods class))
+        has-categories (boolean (seq categories))]
     (assoc class
-           :categories categories
-           :has-categories (boolean (seq categories)))))
+           :categories (if has-categories
+                         (categorize-class-methods (:methods class)))
+           :has-categories has-categories)))
 
 (defn build-namespace-categories [namespace]
   (let [categories (get-namespace-categories (:functions namespace))
         has-categories (boolean (seq categories))]
     (assoc namespace
            :categories (if has-categories
-                         (categorize-members (:functions namespace)))
+                         (categorize-namespace-functions (:functions namespace)))
            :has-categories has-categories)))
