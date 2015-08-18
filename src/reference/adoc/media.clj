260ee48b336f2ef50f95c5e7ee353413e9fbb78c
batsuev <batsuev@gmail.com>
2015-08-18 17:03:19 +0800

    fix media

--- src/reference/adoc/media.clj
+++ src/reference/adoc/media.clj
@@ -17,17 +17,19 @@
   (clojure.string/replace path #"[^/]+$" ""))
 
 (defn- move-file [base-path version path dest-path]
-  (let [target-name (clojure.string/replace path #"_media/" "")
+  (let [target-name (-> path
+                        (clojure.string/replace #"_media/" "")
+                        (clojure.string/replace (clojure.string/re-quote-replacement base-path) ""))
         target-path (str dest-path version "/" target-name)
         target-folder (dirname target-path)]
     (git/run-sh "mkdir" "-p" target-folder)
     (git/run-sh "cp" (str base-path path) target-path)))
 
 (defn move-media [version src-path dest-path]
-  (info "moving media for" version)
-  (let [base-path (str src-path "/" version "/")
+  (info "moving media for" version "from" src-path "to" dest-path)
+  (let [base-path (str src-path version "/")
         files (get-all-static base-path)]
-    (doall (map #(move-file base-path version % dest-path) files))))
+    (doall (map #(move-file base-path version % (str dest-path "/")) files))))
 
 (defn update-links [description version]
   (if-not (empty? description)
