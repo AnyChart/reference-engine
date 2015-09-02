1247b86a0c2fced92753338b7584ccb163923ebf
AntonBaranchuk <anton@anychart.com>
2015-09-01 18:31:17 -0700

    URLs update

--- src/reference/data/seo.clj
+++ src/reference/data/seo.clj
@@ -1,12 +1,12 @@
 (ns reference.data.seo)
 
 (def entries
-  ["AnyChart - HTML5 charts help you to understand any data"
-   "<a href='http://wwww.anychart.com/products/anychart/overview/'>AnyChart</a> - JavaSript charts is a trusted solution for data visualization"
-   "AnyChart - JavaSript charts for web and mobile"
-   "AnyChart - The most comprehensive JavaScript charting library"
-   "AnyChart - Create interactive HTML5 charts easily for your web projects"
-   "IE 6? Latest Chrome? Mobile browsers? AnyChart just works everywhere!"
+  ["<a href='http://www.anychart.com/products/anychart/overview/'>AnyChart</a> - HTML5 charts help you to understand any data"
+   "<a href='http://www.anychart.com/products/anychart/overview/'>AnyChart</a> - JavaSript charts is a trusted solution for data visualization"
+   "<a href='http://www.anychart.com/products/anychart/overview/'>AnyChart</a> - JavaSript charts for web and mobile"
+   "<a href='http://www.anychart.com/products/anychart/overview/'>AnyChart</a> - The most comprehensive JavaScript charting library"
+   "<a href='http://www.anychart.com/products/anychart/overview/'>AnyChart</a> - Create interactive HTML5 charts easily for your web projects"
+   "IE 6? Latest Chrome? Mobile browsers? <a href='http://www.anychart.com/products/anychart/overview/'>AnyChart</a> just works everywhere!"
    "AnyStock's main purpose is to display financial JavaSript charts"
    "AnyMap is a robust interactive Javascript/HTML5 maps library"])
 
