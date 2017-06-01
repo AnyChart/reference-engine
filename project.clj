90bcfc322150e37c3d27a9112d5d36116796dde6
ilevd <ilevd@mail.ru>
2017-06-01 21:22:25 +0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,22 +1,22 @@
 (defproject reference "3.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [version-clj "0.1.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.443"]
+                 [version-clj "0.1.2"]
                  ;; general
-                 [com.stuartsierra/component "0.2.3"]
-                 [clj-time "0.11.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [clj-time "0.13.0"]
                  [me.raynes/fs "1.4.6"]
-                 [cheshire "5.6.3"]
+                 [cheshire "5.7.1"]
                  ;; templates
                  [selmer "0.8.2"]
                  ;; web
-                 [http-kit "2.1.16"]
-                 [compojure "1.1.9"]
-                 [ring/ring-json "0.3.1"]
-                 [ring/ring-core "1.3.1"]
-                 [enlive "1.1.5"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.6.0"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-core "1.6.1"]
+                 [enlive "1.1.6"]
                  ;; logging
-                 [com.taoensso/timbre "4.0.1"]
+                 [com.taoensso/timbre "4.10.0"]
                  ;; databases
                  [com.taoensso/carmine "2.11.1"]
                  [org.clojure/java.jdbc "0.6.1"]
