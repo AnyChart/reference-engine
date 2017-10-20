a142b0473147fa9315023759fb15af569f746393
ilevd <ilevd@mail.ru>
2017-10-20 14:52:43 +0700

    Fix change version on search page

--- js-src/api/versions.js
+++ js-src/api/versions.js
@@ -1,13 +1,18 @@
 goog.provide("api.versions");
 goog.require("api.utils");
 
-/** */
-api.versions.init = function() {
-    $('.versionselect').on('change', function(){
+
+api.versions.init = function () {
+    $('.versionselect').on('change', function () {
+        var newVersion = $(this).find("option:selected").val();
         var entry = api.utils.getEntryFromURL(location.pathname);
-        if (!entry.length)
-            location.href = "/" + $(this).find("option:selected").val() + "/anychart";
-        else
-            location.href = "/" + $(this).find("option:selected").val() + "/try/" + api.utils.getEntryFromURL(location.pathname);
+        if (location.search.length) {
+            // for ?entry=searchString
+            location.href = "/" + newVersion + "/" + location.search;
+        } else if (entry.length) {
+            location.href = "/" + newVersion + "/try/" + entry;
+        } else {
+            location.href = "/" + newVersion + "/anychart";
+        }
     });
 };
