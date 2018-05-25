c0b10506cff388d90301776454f6c681639defec
ilevd <ilevd@mail.ru>
2018-05-25 14:47:00 +0700

    Tags support

--- src/reference/git.clj
+++ src/reference/git.clj
@@ -1,10 +1,13 @@
 (ns reference.git
   (:require [clojure.java.shell :refer [sh with-sh-env with-sh-dir]]
             [taoensso.timbre :as timbre :refer [info error]]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [reference.util.utils :as utils]))
+
 
 (defn run-sh [& command]
   (apply sh command))
+
 
 (defn file-last-commit-date [base-path path]
   (info "last commit date for " (str base-path path))
@@ -15,6 +18,7 @@
                      (string/trim)
                      read-string))))
 
+
 (defn- run-git [git-ssh path & command]
   (with-sh-env {:GIT_SSH git-ssh}
                (with-sh-dir path
@@ -22,19 +26,24 @@
                               (println command res)
                               (:out res)))))
 
+
 (defn current-commit [git-ssh path]
   (subs (run-git git-ssh path "rev-parse" "HEAD") 0 7))
+
 
 (defn update [git-ssh repo]
   (run-git git-ssh repo "fetch" "-p"))
 
+
 (defn pull [git-ssh repo]
   (run-git git-ssh repo "pull"))
+
 
 (defn clean
   "Remove all ignored, untracked files and directories"
   [git-ssh repo]
   (run-git git-ssh repo "clean" "-fxd"))
+
 
 (defn update-samples [git-ssh repo branch]
   (run-git git-ssh repo "checkout" "--" "*")
@@ -42,11 +51,13 @@
   (run-git git-ssh repo "checkout" branch)
   (run-git git-ssh repo "pull" "origin" branch))
 
+
 (defn commit-samples [git-ssh repo branch]
   (run-git git-ssh repo "pull" "origin" branch)
   (run-git git-ssh repo "add" "-A" "./")
   (run-git git-ssh repo "commit" "-m" "samples update")
   (run-git git-ssh repo "push" "origin" branch))
+
 
 (defn checkout [git-ssh repo version target-path]
   (run-sh "rm" "-rf" target-path)
@@ -54,22 +65,31 @@
   (run-git git-ssh target-path "checkout" version)
   (run-git git-ssh target-path "pull" "origin" version))
 
-(defn remote-branches [git-ssh path pred]
-  (let [raw-lines (string/split (run-git git-ssh path "branch" "-r" "--format='%(refname:short)|-|%(objectname)|-|%(authorname)|-|%(contents:subject)'") #"\n")
-        lines (map #(string/replace % #"'" "") raw-lines)
+
+(defn remote-branches [git-ssh path]
+  (let [branch-lines (string/split (run-git git-ssh path "branch" "-r" "--format='%(refname:short)|-|%(objectname)|-|%(authorname)|-|%(contents:subject)'") #"\n")
+        tag-lines (string/split (run-git git-ssh path "for-each-ref" "--format='%(refname:short)|-|%(objectname)|-|%(authorname)|-|%(contents:subject)'" "refs/tags") #"\n")
+        lines (map (fn [line]
+                     (-> line
+                         (string/replace #"^(')(.*)(')$" "$2") ;; delete start and end quotes ' ;; delete ?origin/"
+                         (string/replace #"^([^/]*)/(.*)$" "$2")))
+                   (sort (distinct (concat branch-lines tag-lines))))
         filtered-lines (filter (fn [s] (and (some? s)
-                                            (not (.contains s "origin/HEAD"))
-                                            (pred s))) lines)
+                                            (seq s)
+                                            (not (string/starts-with? s "HEAD")))) lines)
         branches (map (fn [s]
-                        (let [[raw-name commit author message] (string/split s #"\|-\|")]
-                          {:name    (last (re-matches #"origin/(.+)" raw-name))
+                        (let [[name commit author message] (string/split s #"\|-\|")]
+                          {:name    name
                            :commit  commit
                            :author  author
                            :message message})) filtered-lines)]
     branches))
 
+
 (defn actual-branches-with-hashes [git-ssh path]
-  (remote-branches git-ssh path (constantly true)))
+  (remote-branches git-ssh path))
+
 
 (defn version-branches-with-hashes [git-ssh path]
-  (remote-branches git-ssh path (fn [s] (re-find #"origin/\d+\.\d+\.\d+" s))))
+  (let [all-branches (actual-branches-with-hashes git-ssh path)]
+    (filter #(utils/released-version? (:name %)) all-branches)))
